2989dddeae9598e8e7d7c2220af5c592ed45f3b4
Christian <chris.gm0110@gmail.com>
2021-03-20 18:03:59 -0600

    cambio de titulo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>S.P.S</title>
+  <title>Christian</title>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
